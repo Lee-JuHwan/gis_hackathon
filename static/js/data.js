4b8d93f4db4451532f1bace107f206701d72bb06
Lee-JuHwan <ljk8190@naver.com>
2021-10-13 04:22:42 +0900

    commit 7

--- static/js/data.js
+++ static/js/data.js
@@ -9,9 +9,9 @@ for(step = 1; step<11; step++) {
     var data_lat = document.getElementById('lng'+step).innerText;
 
     // 데이터를 positions 배열에 넣는 부분 특성을 여러개 넣을 수 있음
-    positions.push({content: 'TITLE : ' + data_title +
-                        '<br>ADDRESS : ' + data_address +
-                        '<br>SCORE :' + data_score + '<br>',
+    positions.push({content: '이름 : ' + data_title +
+                        '<br>주소 : ' + data_address +
+                        '<br>추천도 :' + data_score + '<br>',
                     latlng: new kakao.maps.LatLng(data_lat, data_lng)
                     });
 }
